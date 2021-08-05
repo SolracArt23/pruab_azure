a29cab1ef5958aadbefd0db1fcee0006c25a5c8e
ccorredor21 <ccorredor202@gmail.com>
2021-08-05 18:18:08 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,10 +3,6 @@ import os
 from flask import send_from_directory
 vent = Flask(__name__)
 
-@vent.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'favicon.ico', mimetype='image/favicon.png')
 @vent.route('/')
 def Index():
     
